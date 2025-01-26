6337b168fd4e7d1bdb81751eefaa82adcf7e94c9
Yann Muller <yann.muller@gmail.com>
2025-01-26 19:07:46 +0100

    Added Totem keyboard, key labels

---
+++ TOTEM.md
@@ -0,0 +1,27 @@
+## The Totem layout
+
+Only the differences to the main layout are documented here.
+
+![colemak](keymap-drawer/totem_colemak.svg)
+
+> The combo map is found [below](#combos).
+
+The backspace key `BSPC` takes the place of `;:` which are only available as combos. `ESC` is also only available as a combo.
+
+![symbols](keymap-drawer/totem_symbols.svg)
+
+![extend](keymap-drawer/totem_extend.svg)
+
+![nav](keymap-drawer/totem_nav.svg)
+
+![media](keymap-drawer/totem_media.svg)
+This layer is accessed by holding the `TAB` or `Enter` keys or activated by the
+combo `TAB` `A`.
+
+To accommodate the cursor keys in the bottom right, the `PGUP` and `PGDN` keys are moved.
+
+![func](keymap-drawer/totem_func.svg)
+
+
+### Combos
+![colemak combos](keymap-drawer/totem_colemak_combos.svg)
--- draw.sh
+++ draw.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-keymap -c keymap-drawer/corne_config.yaml parse -z config/eyelash_corne.keymap -o keymap-drawer/eyelash_corne.yaml
+keymap -c keymap-drawer/keymap_config.yaml parse -z config/eyelash_corne.keymap -o keymap-drawer/eyelash_corne.yaml
 
-keymap -c keymap-drawer/corne_config.yaml draw -s COLEMK --keys-only -j config/eyelash_corne.json keymap-drawer/eyelash_corne.yaml > keymap-drawer/eyelash_corne_colemak.svg
-keymap -c keymap-drawer/corne_config.yaml draw -s MEDIA -j config/eyelash_corne.json keymap-drawer/eyelash_corne.yaml > keymap-drawer/eyelash_corne_media.svg
-keymap -c keymap-drawer/corne_config.yaml draw -s SYMBOLS -j config/eyelash_corne.json keymap-drawer/eyelash_corne.yaml > keymap-drawer/eyelash_corne_symbols.svg
-keymap -c keymap-drawer/corne_config.yaml draw -s EXTEND --keys-only -j config/eyelash_corne.json keymap-drawer/eyelash_corne.yaml > keymap-drawer/eyelash_corne_extend.svg
-keymap -c keymap-drawer/corne_config.yaml draw -s NAV --keys-only -j config/eyelash_corne.json keymap-drawer/eyelash_corne.yaml > keymap-drawer/eyelash_corne_nav.svg
-keymap -c keymap-drawer/corne_config.yaml draw -s FUNC --keys-only -j config/eyelash_corne.json keymap-drawer/eyelash_corne.yaml > keymap-drawer/eyelash_corne_func.svg
+keymap -c keymap-drawer/keymap_config.yaml draw -s COLEMK --keys-only -j config/eyelash_corne.json keymap-drawer/eyelash_corne.yaml > keymap-drawer/eyelash_corne_colemak.svg
+keymap -c keymap-drawer/keymap_config.yaml draw -s MEDIA -j config/eyelash_corne.json keymap-drawer/eyelash_corne.yaml > keymap-drawer/eyelash_corne_media.svg
+keymap -c keymap-drawer/keymap_config.yaml draw -s SYMBOLS -j config/eyelash_corne.json keymap-drawer/eyelash_corne.yaml > keymap-drawer/eyelash_corne_symbols.svg
+keymap -c keymap-drawer/keymap_config.yaml draw -s EXTEND --keys-only -j config/eyelash_corne.json keymap-drawer/eyelash_corne.yaml > keymap-drawer/eyelash_corne_extend.svg
+keymap -c keymap-drawer/keymap_config.yaml draw -s NAV --keys-only -j config/eyelash_corne.json keymap-drawer/eyelash_corne.yaml > keymap-drawer/eyelash_corne_nav.svg
+keymap -c keymap-drawer/keymap_config.yaml draw -s FUNC --keys-only -j config/eyelash_corne.json keymap-drawer/eyelash_corne.yaml > keymap-drawer/eyelash_corne_func.svg
 
 
-keymap -c keymap-drawer/corne_config.yaml draw -s COLEMK --combos-only -j config/eyelash_corne.json keymap-drawer/eyelash_corne.yaml > keymap-drawer/eyelash_corne_colemak_combos.svg
+keymap -c keymap-drawer/keymap_config.yaml draw -s COLEMK --combos-only -j config/eyelash_corne.json keymap-drawer/eyelash_corne.yaml > keymap-drawer/eyelash_corne_colemak_combos.svg
 
